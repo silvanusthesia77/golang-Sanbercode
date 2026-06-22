3c7256f7eac472271ad94949d7f41767dfaa867f
Silvanus <silvanusthesia1@gmail.com>
2026-06-22 22:02:40 +0700

    done

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Halo, Dunia")
+	latihan()
+	name := "Silvanus Thesia"
+	age := 28
+	hobby := "FootBall"
+	fmt.Printf("Nama Saya %s ,  saya berusia %d tahun , hobby saya adalah %s \n", name, age,hobby )
+}
+func latihan (){
+	var name = "John Doe"
+var age = "28"
+sentence := `Nama Saya Dalah "` + name + `usia saya "` + age + `"`
+fmt.Println()
+fmt.Println(sentence)
+
+
 }
