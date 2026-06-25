03d7cbea31c21f1780abdffd1007192f9b3a8bd5
Silvanus <silvanusthesia1@gmail.com>
2026-06-26 00:31:20 +0700

    Done

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Halo Dunia")
+	// variable
+	name := "wanus thesia"
+	age := 28
+	hobby := "Sports"
+	fmt.Printf("Nama %s , usia %d dan Hobby %s \n", name, age, hobby)
+
+	// Tipe Data
+	// int8 -128 - 127
+	var number int8 = -128
+	fmt.Println(number)
+	num := 2.45
+	fmt.Println(num)
+	// Tipe Data Numerik Desimal
+	data := 2.97
+	fmt.Println(data)
+	isCorrect := true
+	fmt.Println(isCorrect)
+	// Tipe Data string
 }
